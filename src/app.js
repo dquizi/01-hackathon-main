61c6dcc7f84d94a072e145c9f8a3caf28d8c2816
Oleg Zeniukov <zenyuckov@gmail.com>
2025-09-27 18:45:41 +0300

    addedContextMenu

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import './styles.css'
+import { ContextMenu } from './menu.js'
+import { AlertModule } from './modules/alert'
 
+const menuHTML = new ContextMenu('#menu')
 
 menuHTML.add(new AlertModule())
