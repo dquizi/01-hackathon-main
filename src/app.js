ca304d1723996a5c739543de37030c18baad8357
Oleg Zeniukov <zenyuckov@gmail.com>
2025-09-27 18:41:07 +0300

    addedContextMenu

--- src/app.js
+++ src/app.js
@@ -1,22 +1,7 @@
 import './styles.css'
-
-// import { ContextMenu } from './menu'
-
-// import { Module } from './core/module'
-
-// const that = new ContextMenu()
-
-// document.addEventListener('contextmenu', (event) => {
-//     event.preventDefault();
-//     that.open()
-//     testModule.toHTML()
-// })
-
-// const testModule = new Module('color', 'Меняем цвет')
-
 import { ContextMenu } from './menu.js'
 import { AlertModule } from './modules/alert'
 
-const menu = new ContextMenu('#menu')
+const menuHTML = new ContextMenu('#menu')
 
-menu.add(new AlertModule())
+menuHTML.add(new AlertModule())
