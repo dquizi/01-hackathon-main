f7bdc214b766b2eeeb47b6c3ca05564f4682d07e
Denis Yurik <quizi@bk.ru>
2025-09-27 23:36:30 +0300

    fix sound modul

--- src/modules/sound.modul.js
+++ src/modules/sound.modul.js
@@ -8,12 +8,8 @@ export class SoundModul extends Module {
   trigger() {
     const sounds = ["https://www.fesliyanstudios.com/play-mp3/366", "https://www.fesliyanstudios.com/play-mp3/4896", "https://www.fesliyanstudios.com/play-mp3/382", "https://www.fesliyanstudios.com/play-mp3/384", "https://www.fesliyanstudios.com/play-mp3/385"];
 
-    const soundItem = document.querySelector(`[data-type="sound"]`);
-
-    soundItem.addEventListener('click', () => {
-      const randomIndex = Math.floor(Math.random() * sounds.length)
-      const audio = new Audio(sounds[randomIndex])
-      audio.play()
-    })
+    const randomIndex = Math.round(Math.random() * sounds.length);
+    const audio = new Audio(sounds[randomIndex]);
+    audio.play();
   }
 }
