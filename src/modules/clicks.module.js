5ceeea35342d4bbc54f0548ee4a8182235d53aa4
Denis Yurik <quizi@bk.ru>
2025-09-27 21:03:33 +0300

    +click module 30%

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,19 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
+  constructor() {
+    super('clicks', 'Клики');
+  }
 
+  trigger() {
+    alert('Таймер запущен (5 сек)! Делайте клики!');
+
+    let counter = 0;
+
+    setTimeout();
+
+    document.addEventListener('click', () => {
+      counter += counter;
+    })
+  }
 }
