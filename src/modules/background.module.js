0f473a66572dbb8e41ef9d43d5cd23109e258c37
Denis Yurik <quizi@bk.ru>
2025-09-27 20:20:59 +0300

    test background

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,17 +1,19 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
-    constructor() {
-        super('back-ground-color-change', 'Поменять цвет')
-    }
+  constructor() {
+    super('back-ground-color-change', 'Поменять цвет')
+  }
 
-    trigger() {
-        function getRandomRgb() {
-            const red = Math.floor(Math.random() * 256)
-            const green = Math.floor(Math.random() * 256)
-            const blue = Math.floor(Math.random() * 256)
-            return `rgb(${red}, ${green}, ${blue})`
-        }
-        document.body.style.backgroundColor = getRandomRgb()
+  trigger() {
+    function getRandomRgb() {
+      const red = Math.floor(Math.random() * 256)
+      const green = Math.floor(Math.random() * 256)
+      const blue = Math.floor(Math.random() * 256)
+      return `rgb(${red}, ${green}, ${blue})`
     }
+    document.body.style.backgroundColor = getRandomRgb()
+  }
 }
+
+console.log('test background');
